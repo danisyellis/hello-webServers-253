c6ddd82c7db39fcd55d617de050a6396f58892f4
Danielle Gellis <danisyellis@gmail.com>
2017-05-15 12:02:32 -0700

    adds express&path, basic routing

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "/views/index.html"));
+})
+
+app.listen(8081);
+console.log("I'm listening! Port 8081");
