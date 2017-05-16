d57e641492c3110344e9761b5f127465ca811f9a
Johnathan Mitchell <johnathanmitchell@LGs-MacBook-Pro.local>
2017-05-15 17:59:38 -0700

    Connects main.css files, adds some starter routes in server.js, start html structure

--- server.js
+++ server.js
@@ -7,9 +7,23 @@ var ejs = require('ejs')
 var app = express();
 app.set('view engine', 'ejs')
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "/views/index.html"));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 })
+
+// app.get('/', function(req, res) {
+//     res.render('views/index.ejs');
+// });
+// app.get('/album', function(req, res) {
+//     res.render('views/album.ejs');
+// });
+// app.get('/albums', function(req, res) {
+//     res.render('views/albums.ejs');
+// });
+// app.get('/artist', function(req, res) {
+//     res.render('views/artist.ejs');
+// });
+
 
 app.listen(8081);
 console.log("I'm listening! Port 8081");
