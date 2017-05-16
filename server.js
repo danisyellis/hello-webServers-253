7b519985e849fab0fac23bb018a3dcf154960b4a
Danielle Gellis <danisyellis@gmail.com>
2017-05-16 09:18:31 -0700

    adds error and 404 routing

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ var path = require("path");
 
 var app = express();
 
+app.use(function(err, req, res, next) {
+  console.err(err.stack);
+  res.status(500).send("Something's wrong! Sorry about that.")
+})
+
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "/views/index.html"));
+})
+
+app.use(function(req, res) {
+  res.status(404).send("404")
 })
 
 app.listen(8081);
