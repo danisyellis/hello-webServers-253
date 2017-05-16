91d9d6d31106fcf4069935437dfc87a563155ed0
Johnathan Mitchell <johnathanmitchell@LGs-MacBook-Pro.local>
2017-05-15 17:23:10 -0700

    Adds ejs to package json, adds template view files

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require("express");
 var path = require("path");
+var ejs = require('ejs')
+
+
 
 var app = express();
+app.set('view engine', 'ejs')
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "/views/index.html"));
