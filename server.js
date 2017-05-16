42e7a03dc8a68e03a425d079cb53688b7f2fae40
Danielle Gellis <danisyellis@gmail.com>
2017-05-16 10:13:50 -0700

    adds partials- head and navbar, eliminates index.html

--- server.js
+++ server.js
@@ -10,26 +10,26 @@ app.use(function(err, req, res, next) {
   res.status(500).send("Something's wrong! Sorry about that.")
 })
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "/views/index.html"));
-})
+// app.get("/", function(req, res) {
+//   res.sendFile(path.join(__dirname, "/views/index.html"));
+// })
+
+app.get('/', function(req, res) {
+    res.render('index.ejs');
+});
+app.get('/album', function(req, res) {
+    res.render('album.ejs');
+});
+app.get('/albums', function(req, res) {
+    res.render('albums.ejs');
+});
+app.get('/artist', function(req, res) {
+    res.render('artist.ejs');
+});
 
 app.use(function(req, res) {
   res.status(404).send("404")
-})
-
-// app.get('/', function(req, res) {
-//     res.render('views/index.ejs');
-// });
-// app.get('/album', function(req, res) {
-//     res.render('views/album.ejs');
-// });
-// app.get('/albums', function(req, res) {
-//     res.render('views/albums.ejs');
-// });
-// app.get('/artist', function(req, res) {
-//     res.render('views/artist.ejs');
-// });
+});
 
 app.listen(8081);
 console.log("I'm listening! Port 8081");
