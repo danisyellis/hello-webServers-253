a846491824cbfaa5accdce040253dfe8d8c3091f
Danielle Gellis <danisyellis@gmail.com>
2017-05-16 17:18:48 -0700

    adds some js to ejs files, also adds bodyParser and logger modules

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 var express = require("express");
 var path = require("path");
-var ejs = require('ejs')
+var ejs = require('ejs');
+var bodyParser = require("body-parser");
+var logger = require("morgan");
 
 var app = express();
 app.set('view engine', 'ejs')
+app.use(logger('dev'));
 
 app.use(function(err, req, res, next) {
   console.err(err.stack);
   res.status(500).send("Something's wrong! Sorry about that.")
 })
 
+app.use(bodyParser.urlencoded({
+  extended:true
+}));
+app.use(bodyParser.json());
+
 // app.get("/", function(req, res) {
 //   res.sendFile(path.join(__dirname, "/views/index.html"));
 // })
 
 app.get('/', function(req, res) {
-    res.render('index.ejs');
+  //var artists = ["Jonathan the Great", "The Guild", "Coders", "The Beatles"];
+  var artists = [{name:"Jonathan the Great"}, {name: "The Guild"}];
+    res.render('index.ejs', {
+      artists: artists
+    });
 });
 app.get('/album', function(req, res) {
     res.render('album.ejs');
